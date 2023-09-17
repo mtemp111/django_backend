c34c4903857b5a4abcc54154861bef0cc38231bb
m temp111 <borioglorio@gmail.com>
2023-09-17 19:15:06 +0200

    dynamic views, url routed views, view from proj

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+# def challenge(request):
+    # return HttpResponse("this works !")
+# def january(request):
+#     return HttpResponse("Eat no candy !")
+# def february(request):
+#     return HttpResponse("Work out for at least 1 hour!")
+
+def monthly_challenge(request, month):
+    challenge_text = None
+    if month == 'january':
+        challenge_text = 'dont eat trash, dont be glutonous'
+    elif month == 'february':
+        challenge_text = 'exercise daily, dont be lazy'
+    else:
+        return HttpResponseNotFound
+    return HttpResponse(challenge_text)
+
+
+
