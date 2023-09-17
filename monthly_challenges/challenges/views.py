363492cd90c6d506c3e31932db678ee3c634fd55
Your Name <you@example.com>
2023-09-17 21:21:09 +0200

    before redirect by number

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -9,21 +9,19 @@ from django.http import HttpResponse, HttpResponseNotFound
 # def february(request):
 #     return HttpResponse("Work out for at least 1 hour!")
 monthly_challenges = { 'january':'dont eat trash',
-                      'february' : 'exercise daily'
-                      'march' : 'be good person'
-                      'april' : 'walk green'
+                      'february' : 'exercise daily',
+                      'march':'be good person',
+                      'april' : 'walk green',
                       'may' : 'scooping'}
 
 
 def monthly_challenge(request, month):
-    challenge_text = None
-    if month == 'january':
-        challenge_text = 'dont eat trash, dont be glutonous'
-    elif month == 'february':
-        challenge_text = 'exercise daily, dont be lazy'
-    else:
-        return HttpResponseNotFound
-    return HttpResponse(challenge_text)
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound('This month not supported')
+    
 
 def  monthly_challenge_by_number(request, month):
     return HttpResponse (month)
