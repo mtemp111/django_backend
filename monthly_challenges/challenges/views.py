633b180c84e286b5a8b96ed4687b28cbfa59658d
Your Name <you@example.com>
2023-09-18 15:52:05 +0200

    4008 Tags for, endfor

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -24,14 +24,9 @@ def monthly_challenge(request, month):
     except:
         return HttpResponseNotFound('This month not supported')
 def index(request):
-    list_items = ''
     months = list(monthly_challenges.keys())
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse('month-challenge',args=[month])
-        list_items += f'<li><a href=\"{month_path}\">{capitalized_month}</a></li>'
-    response_data = f'<ul>{list_items}</ul>'
-    return HttpResponse(response_data)
+    return render(request,'challenges/index.html',{
+        'months':months})
     
 
 def  monthly_challenge_by_number(request, month):
